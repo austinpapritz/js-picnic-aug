760b15ee8bead1edaadd37d3419fd7d20cbeb14e
Austin Papritz <austin@papritz.dev>
2022-08-11 18:15:13 -0700

    click toggle for soda cheese grapes bread and watermelon

---
+++ app.js
@@ -0,0 +1,27 @@
+// step 1 grab element from DOM
+
+const soda = document.getElementById('soda');
+const cheese = document.getElementById('cheese');
+const grapes = document.getElementById('grapes');
+const bread = document.getElementById('bread');
+const watermelon = document.getElementById('watermelon');
+// add even listener
+soda.addEventListener('click', () => {
+    soda.classList.toggle('picked');
+});
+
+cheese.addEventListener('click', () => {
+    cheese.classList.toggle('picked');
+});
+
+grapes.addEventListener('click', () => {
+    grapes.classList.toggle('picked');
+});
+
+bread.addEventListener('click', () => {
+    bread.classList.toggle('picked');
+});
+
+watermelon.addEventListener('click', () => {
+    watermelon.classList.toggle('picked');
+});
